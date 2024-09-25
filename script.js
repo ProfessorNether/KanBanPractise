0c7accdea0af29a00f7f29321e7faaf6d427c22b
SLobnixly <n.olmer@sintlucasedu.nl>
2024-09-25 10:21:44 +0200

    expenseform

--- script.js
+++ script.js
@@ -5,4 +5,19 @@ document.addEventListener('DOMContentLoaded', () => { // loads this before every
     const incomeForm = document.getElementById('income-form');
     const incomeList = document.getElementById('income-list');
     const incomeSummary = document.getElementById('income-summary');
+    
+    expenseForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const amount = parseFloat(document.getElementById('amount').value);
+        const category = document.getElementById('category').value;
+        const description = document.getElementById('description').value;
+
+        const expense = { id: Date.now(), amount, category, description };
+        expenses.push(expense);
+        displayExpenses();
+        displaySummary();
+
+        expenseForm.reset();
+    });
 });

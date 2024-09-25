a13115b11a28c9dea085d004031c0f8e34775ce0
Luuk600 <119942867+luuk600@users.noreply.github.com>
2024-09-25 10:19:29 +0200

    adding js and the const in js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => { // loads this before everything else
+    const expenseForm = document.getElementById('expense-form');
+    const expenseList = document.getElementById('expense-list');
+    const expenseSummary = document.getElementById('expense-summary');
+    const incomeForm = document.getElementById('income-form');
+    const incomeList = document.getElementById('income-list');
+    const incomeSummary = document.getElementById('income-summary');
+});

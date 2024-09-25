dd6ee2d490a42cd9213cfd5076f1eda658fe83a7
Luuk600 <119942867+luuk600@users.noreply.github.com>
2024-09-25 10:24:49 +0200

    adding the incomeform in js

--- script.js
+++ script.js
@@ -20,4 +20,21 @@ document.addEventListener('DOMContentLoaded', () => { // loads this before every
 
         expenseForm.reset();
     });
+
+    incomeForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const amount2 = parseFloat(document.getElementById('amount2').value);
+        const category2 = document.getElementById('category2').value;
+        const description2 = document.getElementById('description2').value;
+
+        const income = { id: Date.now(), amount: amount2, category: category2, description: description2 };
+        incomes.push(income);
+       
+
+        displayIncome();
+        displaySummaryIncome();
+        
+        incomeForm.reset();
+    });
 });
